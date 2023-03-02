6de8da11812c07392e40cf2cb3a7d67a2c1e20b2
louis nowak <nowak.louis@gmail.com>
2023-03-02 12:14:27 +0100

    start project

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,27 @@
-import Head from 'next/head'
-import { Stack } from '@mui/material';
-import Navbar from '../components/navbar';
+import Head from "next/head";
+import { Stack } from "@mui/material";
+import NavBar from "../components/NavBar";
 
 export default function Home() {
   return (
     <>
       <Head>
         <title>Wild Wood</title>
-        <link rel="icon" href="/favico.png" />
+        <link rel="icon" href="/WildFavicon.png" />
       </Head>
       <Stack
         sx={{
-          minHeight: '100vh',
-          minWidth: '100vw',
-          padding: '1rem',
-          backgroundImage: 'url("./boardwood.jpg")',
-          backgroundSize: 'cover',
-          backgroundAttachment: 'fixed',
+          minHeight: "100vh",
+          minWidth: "100vw",
+          padding: "1rem",
+          backgroundImage:
+            "linear-gradient(to top, #edf1d6, #cddab6, #abc498, #87ae7d, #609966)",
+          backgroundSize: "cover",
+          backgroundAttachment: "fixed",
         }}
       >
-        <Navbar />
+        <NavBar />
       </Stack>
     </>
-  )
+  );
 }
